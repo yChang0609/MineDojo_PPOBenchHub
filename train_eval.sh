40c2dc8adb4cb9fc2aa786db51e47e6b624e2c46
yChang0609 <ychang0609@gmail.com>
2025-01-17 17:22:07 +0800

    Hunt Cows task

--- train_eval.sh
+++ train_eval.sh
@@ -67,6 +67,9 @@
 #     "logs/HuntCowEasy/100K_CLIP_HuntCow-OnlyCow/PPO" 
 
 # MINEDOJO_HEADLESS=1 python -u train.py --config configs/1M_CLIP_HarvestMilk.yaml
+# MINEDOJO_HEADLESS=1 
+python -u train.py --config configs/hunt_cows/100K_CLIP_HuntCows.yaml
 
-MINEDOJO_HEADLESS=1 python -u eval.py -logs \
-    "logs/ppo_HarvestMilk/1M_CLIP_HarvestMilk/PPO_1" 
+
+# MINEDOJO_HEADLESS=1 python -u eval.py -logs \
+#     "logs/ppo_HarvestMilk/1M_CLIP_HarvestMilk/PPO_1" 
