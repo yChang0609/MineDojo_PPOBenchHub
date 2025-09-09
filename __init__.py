4a0b4d6efec297c253abe22b97467767d6ea925a
yChang0609 <ychang0609@gmail.com>
2025-09-09 19:20:59 +0800

    build_env package path

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ import sys
 import os
 project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(project_root)
-from env_factory import build_env
+from src.env_factory import build_env
